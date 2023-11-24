64a6b06d3010e0bfa9ab93b6fdd1f326875c22a6
daniel <dramirezn18@gmail.com>
2023-11-24 12:03:04 -0500

    Implementar POO en ejercicio practico

--- main.js
+++ main.js
@@ -1,51 +1,145 @@
-const natalia = {
-    name: "Natalia",
-    age: 20,
-    cursosAprobados: [
-        "Curso definitivo de HTML y CSS",
-        "Curso practico de HTML y CSS"
-    ],
-    aprobarCurso(nuevoCursito) {
-        this.cursosAprobados.push(nuevoCursito);
-    },
-}
-
-natalia.cursosAprobados.push("Curso de Responsive Design");
-
-function Student(name, age, cursosAprobados) {
-    this.name = name;
-    this.age = age;
-    this.cursosAprobados = cursosAprobados;
-    // this.aprobarCurso = function(nuevoCursito) {
-    //     this.aprobarCurso.push(nuevoCursito);
-    // }
-}
-Student.prototype.aprobarCurso = function(nuevoCursito) {
-    this.cursosAprobados.push(nuevoCursito);
-}
-
-const juanita = new Student("Juanita", 20, ["Curso basico de JavaScript", "Curso de programación orientada a objetos con C#"]);
-
-class Student2 {
+class Course {
     constructor({
-        cursosAprobados = [],
-        age,
-        email,
         name,
+        difficultyLevel,
     }) {
         this.name = name;
-        this.age = age;
-        this.cursosAprobados = cursosAprobados;
-        this.email = email;
-    }
-
-    aprobarCurso(nuevoCursito) {
-        this.cursosAprobados.push(nuevoCursito);
+        this.difficultyLevel = difficultyLevel;
     }
 }
 
-const miguel = new Student2({
+const course1 = new Course({
+    name: "Curso definitivo de HTML y CSS",
+    difficultyLevel: "Basico",
+});
+
+const course2 = new Course({
+    name: "Curso practico de HTML y CSS",
+    difficultyLevel: "Basico",
+});
+
+const course3 = new Course({
+    name: "Curso de DataBusiness",
+    difficultyLevel: "Basico",
+});
+
+const course4 = new Course({
+    name: "Curso de DataViz",
+    difficultyLevel: "Intermedio",
+});
+
+const course5 = new Course({
+    name: "Curso introducción a la producción de videojuegos",
+    difficultyLevel: "Basico",
+});
+
+const course6 = new Course({
+    name: "Curso de Unreal Engine",
+    difficultyLevel: "Intermedio",
+});
+
+const course7 = new Course({
+    name: "Curso de Unity 3D",
+    difficultyLevel: "Intermedio",
+});
+
+
+
+class LearningPath {
+    constructor({
+        name,
+        courses = []
+    }) {
+        this.name = name;
+        this.courses = courses;
+    }
+
+    addCourse(course) {
+        this.courses.push(course);
+    }
+}
+
+
+// Rutas de aprendizaje
+const webSchool = new LearningPath({
+    name: "Escuela de desarrollo web",
+    courses: [
+        course1,
+        course2,
+    ]
+});
+
+const dataScienceSchool = new LearningPath({
+    name: "Escuela de Data Science",
+    courses: [
+        course3,
+        course4,
+    ]
+});
+
+const gameSchool = new LearningPath({
+    name: "Escuela de desarrollo de videojuegos",
+    courses: [
+        course5,
+        course6,
+        course7,
+    ]
+});
+
+
+class Student {
+    constructor({
+        name,
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = [],
+    }) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.twitter = twitter;
+        this.instagram = instagram;
+        this.facebook = facebook;
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+    }
+}
+
+const Miguel = new Student({
     name: "Miguel",
-    email: "miguel30@platzi.com",
-    age: 30,
+    email: "miguel@platzi.com",
+    learningPaths: [
+        webSchool,
+        gameSchool,
+    ],
+    username: "miguel",
+    twitter: "miguelPro",
+    approvedCourses: [
+        course1,
+        course2,
+        course5,
+        course6,
+        course7,
+    ]
+});
+
+const Oscar = new Student({
+    name: "Oscar",
+    email: "oscar30@platzi.com",
+    learningPaths: [
+        webSchool,
+        dataScienceSchool,
+    ],
+    username: "oscar",
+    twitter: "oscarfeliz",
+    approvedCourses: [
+        course1,
+        course2,
+        course3,
+        course4,
+    ]
 });
